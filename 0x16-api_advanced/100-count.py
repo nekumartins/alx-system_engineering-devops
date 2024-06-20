b4b7fa8f1ba3c3bde41e741d33d3d61c3746f28b
Chukwuneku <akpotohwoo@gmail.com>
2024-06-20 02:21:19 +0100

    fix

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -14,7 +14,7 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
     """
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Mean-Cranberry7153)"
+        "User-Agent": "linux:0x16.alx:v1.0.0 (by /u/Mean-Cranberry7153)"
     }
     params = {
         "after": after,
